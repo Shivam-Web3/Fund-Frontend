8ca3520f140a0b2d68d5b09277c52b5c57f31fb8
Shivam-Web3 <109238835+Shivam-Web3@users.noreply.github.com>
2022-07-30 10:35:53 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0x8B46D0B519Cdf30DDFdccB5F3487bB7728CEeb86"
+export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 export const abi = [
    {
